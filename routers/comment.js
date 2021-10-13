ab685c02661774ba8c85b7bf4eeab118bb29031c
leubenia <hdodo777@gmail.com>
2021-10-14 00:20:41 +0900

    코맨트 완료

--- routers/comment.js
+++ routers/comment.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const Comment = require("../models/comment");
-require("date-utils");
+const authMiddleware = require("../middlewares/auth-middleware");
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
 // postId 검증은 내일하자...!
 
@@ -17,12 +23,12 @@ router.get("/:postId", async (req, res) => {
   }
 });
 
-router.post("/:postId", async (req, res) => {
+router.post("/:postId",authMiddleware ,async (req, res) => {
   const { postId } = req.params;
   const { nickname, comment } = req.body;
   let newDate = new Date();
   let date = newDate.toFormat("YYYY-MM-DD HH24:MI:SS");
-  let userId;
+
   try {
     const post = `INSERT INTO comment (commentTime, nickname, comment, upperPost ) VALUES ("${date}","${nickname}", "${comment}", "${postId}");`;
     db.query(post, req.body, (error, results, fields) => {
@@ -37,21 +43,12 @@ router.post("/:postId", async (req, res) => {
   }
 });
 
-router.patch("/:commentId", async (req, res) => {
+router.patch("/:commentId",authMiddleware ,async (req, res) => {
   const { commentId } = req.params;
-  const { email, comment } = req.body;
-  
+  const { comment } = req.body;
+  const user = res.locals.user;
   try {
-    const post = `UPDATE user SET comment= "${comment}" WHERE commentId = ${commentId};`;
-    db.query(post, req.body, (error, results, fields) => {
-      if (error) {
-        res.status(400).send(error);
-      } else {
-        res.status(200).send({ results });
-      }
-    });
-    
-    const post = `UPDATE comment SET comment= "${comment}" WHERE commentId = ${commentId};`;
+    const post = `UPDATE comment SET comment= "${comment}" WHERE commentId = ${commentId} AND nickname = ${user.nickname};`;
     db.query(post, req.body, (error, results, fields) => {
       if (error) {
         res.status(400).send(error);
@@ -64,10 +61,11 @@ router.patch("/:commentId", async (req, res) => {
   }
 });
 
-router.delete("/:commentId", async (req, res) => {
+router.delete("/:commentId", authMiddleware,async (req, res) => {
   const { commentId } = req.params;
+  const user = res.locals.user;
   try {
-    const post = `DELETE FROM comment WHERE commentId = ${commentId};`;
+    const post = `DELETE FROM comment WHERE commentId = ${commentId} AND nickname = ${user.nickname};`;
     db.query(post, req.body, (error, results, fields) => {
       if (error) {
         res.status(400).send(error);
