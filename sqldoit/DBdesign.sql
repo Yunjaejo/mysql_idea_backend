6db939d7f8fed27acc88f113bcc3a5ffa6eb973c
yunjaejo <1a616@naver.com>
2021-10-13 21:55:11 +0900

    DB모델링

--- sqldoit/DBdesign.sql
+++ sqldoit/DBdesign.sql
@@ -1,39 +1,41 @@
 
 create table user(
-	userId int(11) not null auto_increment,
+	userId int(20) not null auto_increment,
 	email varchar(50) not null unique,
-	password varchar(40) not null unique,
-    nickName varchar(40) not null unique,
+	password varchar(40) not null,
+    nickname varchar(40) not null unique,
 	primary key(userId),
 	index(email)
 );
 create table post(
-	postId int(11) not null auto_increment,
+	postId int(20) not null auto_increment,
 	title varchar(50) not null,
+	nickname varchar(40),
 	spec varchar(50),
 	descr varchar(50),
-	image varchar(255),
+	image text,
 	place int(11) not null,
-	primary key(postId),
-	index(title)
+    primary key(postId),
+    foreign key(nickname)REFERENCES user(nickname) ON UPDATE CASCADE,
+    index(title)
 );
 create table comment(
-	commentId int(11) not null auto_increment,
-	commentTime varchar(50) not null,
-	nickname varchar(50) not null,
-	comment varchar(50) not null,
-	upperPost int(20) not null,
-	primary key(commentId),
-	foreign key(upperPost)REFERENCES post(postId) ON DELETE CASCADE ON UPDATE CASCADE,
-    foreign key(nickname)REFERENCES user(nickname) ON DELETE CASCADE ON UPDATE CASCADE,
-	index(nickname)
+    commentId int(20) not null auto_increment,
+    commentTime varchar(50) not null,
+    nickname varchar(50) not null,
+    comment varchar(50) not null,
+    upperPost int(20) not null,
+    primary key(commentId),
+    foreign key(upperPost)REFERENCES post(postId) ON DELETE CASCADE ON UPDATE CASCADE,
+    foreign key(nickname)REFERENCES user(nickname) ON UPDATE CASCADE,
+    index(nickname)
 );
 create table wish(
-	Id int(11) not null auto_increment,
-	userId int(20) not null,
-	postId int(20) not null,
-	primary key(Id),
-	foreign key(postId)REFERENCES post(postId) ON DELETE CASCADE ON UPDATE CASCADE,
+    Id int(11) not null auto_increment,
+    userId int(20) not null,
+    postId int(20) not null,
+    primary key(Id),
+    foreign key(postId)REFERENCES post(postId) ON DELETE CASCADE ON UPDATE CASCADE,
     foreign key(userId)REFERENCES user(userId) ON DELETE CASCADE ON UPDATE CASCADE,
-	index(userId)
+    index(userId)
 );
