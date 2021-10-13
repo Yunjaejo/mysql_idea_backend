61c161f1810b216267d62aafbb67adb059a122cf
yunjaejo <1a616@naver.com>
2021-10-13 16:47:45 +0900

    연결 성공

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const cookieParser = require('cookie-parser');
 const app = express(); // 익스프레스 쓸때는 app이라고 명시
 app.use(cookieParser()); // 쿠키값을 꺼낼 수 있음
 const port = 3000;
-const authMiddleware = require('./middlewares/auth-middleware');
+// const authMiddleware = require('./middlewares/auth-middleware');
 const cors = require('cors');
+const mysql = require('mysql');
 
-const connect = require('./models'); // models 폴더 참조
-connect(); // 모델이랑 연결하기
+const dotenv = require("dotenv")
+dotenv.config()
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+db.connect();
+
+db.query(`SHOW TABLES`, (error, results) => {
+  if(error) {
+    console.log(error);
+  }
+  console.log(results);
+})
 
 // const options = {
 //   origin: 'http://example.com', // 접근 권한을 부여하는 도메인
@@ -19,15 +35,15 @@ connect(); // 모델이랑 연결하기
 app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // POST로 메소드 받을 때 req.body로 사용가능하게 함
-
-const postRouter = require('./routers/post');
-const userRouter = require('./routers/user');
-const wishRouter = require('./routers/wish');
-const commRouter = require('./routers/comment');
-app.use('/post', [ postRouter ]); // postRouter를 api 하위부분에서 쓰겠다 !
-app.use('/', [ userRouter ]);
-app.use('/comment', [ commRouter ]);
-app.use('/wish', [ wishRouter ]);
+//
+// const postRouter = require('./routers/post');
+// const userRouter = require('./routers/user');
+// const wishRouter = require('./routers/wish');
+// const commRouter = require('./routers/comment');
+// app.use('/post', [ postRouter ]); // postRouter를 api 하위부분에서 쓰겠다 !
+// app.use('/', [ userRouter ]);
+// app.use('/comment', [ commRouter ]);
+// app.use('/wish', [ wishRouter ]);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${ port }`);
